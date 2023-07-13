d15a592f87e72a30f13c87c0c1680b67cbd1f248
mkaufmanaespjcom <137336312+mkaufmanaespjcom@users.noreply.github.com>
2023-07-13 16:36:45 -0400

    Update and rename data-7-13-2023-0423.js to data-7-13-2023-0424.js

--- jobBoardGithub/data/data-7-13-2023-0424.js
+++ jobBoardGithub/data/data-7-13-2023-0424.js
@@ -65,7 +65,8 @@ let jobsdata = [  /* You can add more positions and information based on the for
         <li>Act as a team leader, communicating project requirements and expectations, working closely with others to achieve your projects’ goals and schedules</li>
         <li>Responsibly balance available manpower and resources as required to meet your projects’ goals and schedule</li>
     </ul>
-
+    <br>
+    
     <p>Requirements and Recommendations</p>
     <ul>
         <li>PE or SE license</li>
@@ -76,7 +77,8 @@ let jobsdata = [  /* You can add more positions and information based on the for
         <li>Recommended - BIM (Revit) Experience</li>
         <li>Recommended - Masters degree in engineering or equivalent program</li>
     </ul>
-
+    <br>
+    
     <p>Benefits</p>
     <ul>
         <li>Competitive compensation package that includes holiday and paid time off</li>
@@ -86,7 +88,8 @@ let jobsdata = [  /* You can add more positions and information based on the for
         <li>Voluntary Aflac dental and supplemental insurance policies</li>
         <li>Participation in a self-directed 401K plan with matching</li>
     </ul>
-
+    <br>
+    
     <p>Perks</p>
     <ul>
         <li>Hands-on training and support</li>
@@ -96,6 +99,7 @@ let jobsdata = [  /* You can add more positions and information based on the for
         <li>Community outreach opportunities</li>
         <li>Flexible work environment - work from home options available</li>
     </ul>
+    <br>
     
     <p><i>AES is an Equal Opportunity Employer. AES does not discriminate on the basis of race, religion, color, sex, gender identity, sexual orientation, 
         age, nondisqualifying physical or mental disability, national origin, veteran status, or any other basis covered by appropriate law. 
@@ -103,6 +107,61 @@ let jobsdata = [  /* You can add more positions and information based on the for
       "location": "Pittsburgh, PA",
       "locationSlug": "pittsburgh-pa",
       "department": "Drafting",
+      "style": ["bulletPNT"]
+    },
+    {
+      "id": 2,
+      "title": "CADD/BIM Tech",
+      "description": `<p>Atlantic Engineering Services (AES) is seeking a full-time CADD/BIM Technician to support our growing team. With offices in 
+        Pittsburgh, PA; Morgantown, WV; and Jacksonville, FL; AES is an award-winning, full-service structural engineering firm with over 30 years of experience. 
+        Our exceptional team of professionals are passionate about serving our clients, providing engineering expertise to the architectural design and construction community. 
+        Providing consulting services to a wide array of industries across the east coast, professionals at the firm craft structures in which people live, learn, 
+        play, heal, worship, and more. As a CADD/BIM Technician, you will work alongside our structural engineers and architectural clients to prepare construction 
+        documents using Revit and AutoCAD software.</p>
+
+    <p>Requirements</p>
+    <ul>
+        <li>A minimum of 3 years of experience in the Revit production of structural systems and drawings for various types of projects including commercial, 
+            industrial, institutional, healthcare, and residential construction</li>
+
+        <li>Experience in the structural drawing preparation process for composite steel, concrete, tilt-up, masonry, and wooden buildings</li>
+        <li>Experience with Revit and AutoCAD</li>
+        <li>Recommendations</li>
+        <li>Technical Associates Degree</li>
+    </ul>
+    <br>
+    
+    <p>Benefits</p>
+    <ul>
+        <li>Competitive compensation package that includes holiday and paid time off</li>
+        <li>Employer assistance in purchasing company health and vision insurances</li>
+        <li>Employer-paid long term disability insurance</li>
+        <li>Employer-paid life insurance</li>
+        <li>Voluntary Aflac dental and supplemental insurance policies</li>
+        <li>Participation in self-directed 401K plan with matching</li>
+    </ul>
+    <br>
+    
+    <p>Perks</p>
+    <ul>
+        <li>Hands-on training and support</li>
+        <li>Advancement opportunities</li>
+        <li>Financial advisement services for investments and retirement options</li>
+        <li>Company trips and events</li>
+        <li>Community outreach opportunities</li>
+        <li>Flexible work opportunities, work-from-home options possible</li>
+    </ul>
+    <br>
+    <p>Location Options</p>
+        <li>Position available through either the Pittsburgh, Jacksonville, and Morgantown office locations</li>
+    <br>
+
+    <p><i>AES is an Equal Opportunity Employer. AES does not discriminate on the basis of race, religion, color, sex, gender identity, sexual orientation, 
+        age, non-disqualifying physical or mental disability, national origin, veteran status, or any other basis covered by appropriate law. 
+        All employment is decided on the basis of qualifications, merit, and business need.</i></p>`,
+      "location": "Pittsburgh, PA",
+      "locationSlug": "pittsburgh-pa",
+      "department": "Engineering",
       "style": ["bulletPNT"]
     }
   ]
